c518860e5426636b26f13b864e0b63d26ca95098
Hadar Dagan <hadarda11@gmail.com>
2019-12-07 14:57:40 +0200

    DR x and y

--- tetris_grid.py
+++ tetris_grid.py
@@ -2,7 +2,7 @@ import pygame
 from pygamepp.grid import Grid
 
 from colors import Colors
-from tetris_piece import Piece
+from pieces.tetris_piece import Piece
 
 
 class TetrisGrid(Grid):
@@ -13,50 +13,48 @@ class TetrisGrid(Grid):
         block_size = 50
         for row in self.blocks:
             for block in row:
-                block_proportions = block.position
                 pygame.draw.line(screen, Colors.GREY,
-                                 block_proportions, (block_proportions[0] + 10,
-                                                     block_proportions[1]))
+                                 (block.x, block.y), (block.x + 10,
+                                                     block.y))
                 pygame.draw.line(screen, Colors.DARK_GREY,
-                                 (block_proportions[0] + 10, block_proportions[1]),
-                                 (block_proportions[0] + block_size - 10, block_proportions[1]))
+                                 (block.x + 10, block.y),
+                                 (block.x + block_size - 10, block.y))
                 pygame.draw.line(screen, Colors.GREY,
-                                 block_proportions, (block_proportions[0],
-                                                     block_proportions[1] + 10))
+                                 (block.x, block.y), (block.x,
+                                                     block.y + 10))
                 pygame.draw.line(screen, Colors.DARK_GREY,
-                                 (block_proportions[0], block_proportions[1] + 10),
-                                 (block_proportions[0], block_proportions[1] + block_size - 10))
+                                 (block.x, block.y + 10),
+                                 (block.x, block.y + block_size - 10))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0] + block_size, block_proportions[1]),
-                                 (block_proportions[0] + block_size - 10, block_proportions[1]))
+                                 (block.x + block_size, block.y),
+                                 (block.x + block_size - 10, block.y))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0] + block_size, block_proportions[1]),
-                                 (block_proportions[0] + block_size, block_proportions[1] + 10))
+                                 (block.x + block_size, block.y),
+                                 (block.x + block_size, block.y + 10))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0] + block_size,
-                                  block_proportions[1] + block_size)
-                                 , (block_proportions[0] + block_size - 10,
-                                    block_proportions[1] + block_size))
+                                 (block.x + block_size,
+                                  block.y + block_size)
+                                 , (block.x + block_size - 10,
+                                    block.y + block_size))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0] + block_size,
-                                  block_proportions[1] + block_size),
-                                 (block_proportions[0] + block_size,
-                                  block_proportions[1] + block_size - 10))
+                                 (block.x + block_size,
+                                  block.y + block_size),
+                                 (block.x + block_size,
+                                  block.y + block_size - 10))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0], block_proportions[1] + block_size),
-                                 (block_proportions[0] + 10, block_proportions[1] + block_size))
+                                 (block.x, block.y + block_size),
+                                 (block.x + 10, block.y + block_size))
                 pygame.draw.line(screen, Colors.GREY,
-                                 (block_proportions[0], block_proportions[1] + block_size),
-                                 (block_proportions[0], block_proportions[1] + block_size - 10))
+                                 (block.x, block.y + block_size),
+                                 (block.x, block.y + block_size - 10))
                 pygame.draw.line(screen, Colors.DARK_GREY,
-                                 (block_proportions[0] + 10, block_proportions[1] + block_size),
-                                 (block_proportions[0] + block_size - 10,
-                                  block_proportions[1] + block_size))
+                                 (block.x + 10, block.y + block_size),
+                                 (block.x + block_size - 10,
+                                  block.y + block_size))
 
     def reset_screen(self, screen: pygame.Surface):
         screen.fill(Colors.BLACK)
         self.display_borders(screen)
-
 
     def freeze_piece(self, piece: Piece):
         for pos in piece.position:
