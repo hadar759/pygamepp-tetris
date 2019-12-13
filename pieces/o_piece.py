8b72dc7c2535ccd55c5d98124d9e3213fbc4cf04
Hadar Dagan <hadarda11@gmail.com>
2019-12-13 15:52:41 +0200

    no rotating :(

--- pieces/o_piece.py
+++ pieces/o_piece.py
@@ -9,33 +9,7 @@ class OPiece(Piece):
         super().__init__(self.sprite, [[0, 4], [0, 5], [1, 5], [1, 4]])
 
     def rotate_clockwise(self, grid):
-        illegal_rotation = False
-        old_positions = self.position[:]
-        pivot_point = self.position[1]
-        for i in range(len(self.position)):
-            self.position[i] = self.rotated_piece_position(self.position[i],
-                                                           pivot_point,
-                                                           self.CLOCKWISE_TRANSFORMATION_MATRIX)
-            if (self.position[i][1] < self.LEFT_BORDER
-                    or self.position[i][1] > self.RIGHT_BORDER
-                    or self.position[i][0] > self.LOWER_BORDER
-                    or grid.blocks[self.position[i][0]][self.position[i][1]].occupied):
-                illegal_rotation = True
-        if illegal_rotation:
-            self.position = old_positions
+        pass
 
     def rotate_counter_clockwise(self, grid):
-        illegal_rotation = False
-        old_positions = self.position[:]
-        pivot_point = self.position[1]
-        for i in range(len(self.position)):
-            self.position[i] = self.rotated_piece_position(self.position[i],
-                                                           pivot_point,
-                                                           self.COUNTER_CLOCKWISE_TRANSFORMATION_MATRIX)
-            if (self.position[i][1] < self.LEFT_BORDER
-                    or self.position[i][1] > self.RIGHT_BORDER
-                    or self.position[i][0] > self.LOWER_BORDER
-                    or grid.blocks[self.position[i][0]][self.position[i][1]].occupied):
-                illegal_rotation = True
-        if illegal_rotation:
-            self.position = old_positions
+        pass
